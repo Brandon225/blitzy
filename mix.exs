34de7452ad6c2d2e1ef202d5977c6ddf5238e565
Brandon Tindle <bjtactor@me.com>
2018-06-16 14:13:07 -0400

    Blitzy complete!

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Blitzy.MixProject do
       version: "0.1.0",
       elixir: "~> 1.4",
       escript: [main_module: Blitzy.CLI],
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
